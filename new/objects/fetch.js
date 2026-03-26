efc9cc0243b33175f1fd5702bd87ccbcb56fb3e4
kamaukimani <paulkimys285@gmail.com>
2026-03-26 17:40:20 +0300

    update file

--- new/objects/fetch.js
+++ new/objects/fetch.js
@@ -4,6 +4,20 @@ const init = () => {
     console.log(form);
     form.addEventListener("submit",function(e){
         e.preventDefault();
+        //console.log(e.target.children[1].value);
+        const input=document.querySelector("#searchByID")
+        console.log(input.value);
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response=>response.json())
+        .then(data=>{
+            const title=document.querySelector("section#movieDetails h4");
+            
+            title.textContent=data['title'];
+            console.log(title);
+            const summary=document.querySelector("section#movieDetails p");
+            summary.innerText=data.summary;
+            console.log(summary);
+        });
     })
   
 }
