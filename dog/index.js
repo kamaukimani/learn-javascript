edafd394e31fbc5cafcb3ae80cda09fce65932ff
kamaukimani <paulkimys285@gmail.com>
2026-04-07 09:12:58 +0300

    update file with binary search algorithm

--- dog/index.js
+++ dog/index.js
@@ -31,3 +31,32 @@ function palindrome(word){
         return true;
     }
 }
+function findStock(sortedArray){
+    let start=0;
+    let end=sortedArray.length;
+    while(start<=end){
+        let mid=Math.floor((start+end)/2)
+        if(sortedArray[mid]==="sock") return "sock";
+        if(sortedArray[mid]< "sock") {
+            start=mid+1;
+        }else{
+            end=mid-1
+        }
+    }
+}
+
+
+function binarySearch(arr,target){
+    let start=0;
+    let end=arr.length-1;
+    while(start<=end){
+        let mid=Math.floor((start+end)/2);
+        if(arr[mid]===target)return target;
+        if(arr[mid]<target){
+            start=mid+1;
+        }else{
+            end=mid-1;
+        }
+    }
+};
+const nameArray=['Alex','Brian','Cate','David','Ephantus','Felix','Gabriel','Hana']
