3fbc400a86265bff58cac59b8300d23d7b0775b2
kamaukimani <paulkimys285@gmail.com>
2026-04-24 17:48:23 +0300

    update fike with solution to code-along

--- class/index.js
+++ class/index.js
@@ -248,3 +248,28 @@ class Square2{
 }
 const square2=new Square2(7);
 const side2=square2.sideLength;
+
+//code along
+class Circle{
+    constructor(radius){
+        this.radius=radius
+    }
+    get diameter(){
+        return  this.radius*2;
+    }
+    get circumference(){
+        return Math.PI*this.diameter;
+    }
+    get area(){
+        return Math.PI*this.radius*this.radius;
+    }
+    set diameter(diameter){
+        this.radius=diameter/2;
+    }
+    set circumference(circumference){
+        this.radius=circumference/(2*Math.PI)
+    }
+    set area(area){
+        this.radius=Math.sqrt(area/Math.PI)
+    }
+}
