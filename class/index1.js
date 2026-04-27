a210e79617312d83bd060355cf878bac9df35adb
kamaukimani <paulkimys285@gmail.com>
2026-04-27 13:02:05 +0300

    include the solution to the lab challenge

--- class/index1.js
+++ class/index1.js
@@ -176,6 +176,42 @@ const creature =new Pet1("The thing");
 const dog1=new Dog1("Spot","foxhound");
 
 //calling the static methods 
-Pet1.definition();
+Pet1.definition(); 
 Dog1.definition();
 
+//super lab
+class Tree{
+    constructor(species){
+        this.species=species;
+    }
+    static definition(){
+        return `A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.`;
+    }
+}
+const tree=new Tree("Maple");
+
+class Deciduous extends Tree{
+    constructor(species,name){
+        super(species);
+        this.name=name;
+    }
+    static definition(){
+        return(
+            super.definition() + "Deciduous trees shed their leaves annually."
+        )
+    }
+}
+const oak = new Deciduous( 'Oak', 'Professor' );
+
+class Evergreen extends Tree{
+    constructor(species,name){
+        super(species);
+        this.name=name;
+    }
+    static definition(){
+        return(
+            super.definition() + "Evergreens keep their leaves all year round."
+        )
+    }
+}
+const fir = new Evergreen( 'Fir', 'Douglas' );
