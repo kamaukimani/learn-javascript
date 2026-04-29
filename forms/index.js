7e0a668f7d5e197150ae0c46a68860f3da92cce4
kamaukimani <paulkimys285@gmail.com>
2026-04-29 22:36:30 +0300

    update files

--- forms/index.js
+++ forms/index.js
@@ -1,1 +1,53 @@
 console.log("Are we connected to learn on forms???");
+const form=document.querySelector("#form");
+console.log(form);
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    const owner=document.querySelector("#owner").value;
+    //console.log(owner);
+    const address=document.querySelector("#address").value;
+    //console.log(address);
+    const dogName=document.querySelector("#dogName").value;
+    //console.log(dogName);
+    const dogAge=document.querySelector("#dogAge").value;
+    //console.log(dogAge);
+    fetch("http://localhost:3000/dogs",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+            "Accept":"application/json"
+        },
+        body:JSON.stringify({
+            owner:owner,
+            address:address,
+            dogName:dogName,
+            dogAge:dogAge
+        })
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+        form.reset()
+    })
+    .catch(error=>console.log(error.message))
+})
+const formGet=document.querySelector("#form-get");
+console.log(formGet);
+formGet.addEventListener("submit",(event)=>{
+    event.preventDefault()
+    const owner=document.querySelector("#owner-get").value;
+    console.log(owner);
+    const address=document.querySelector("#address-get").value;
+    console.log(address);
+    const dogName=document.querySelector("#dogName-get").value;
+    console.log(dogName);
+    const dogAge=document.querySelector("#dogAge-get").value;
+    console.log(dogAge);
+    fetch(`http://localhost:3000/dogs?owner=${owner}&address=${address}&dogName=${dogName}&dogAge=${dogAge}`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+        formGet.reset()
+    })
+    .catch(error=>console.log(error.message))
+ })
