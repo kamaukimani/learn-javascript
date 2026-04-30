7acc6b03a3721eeae607791f1cf51457f1fbb9a7
kamaukimani <paulkimys285@gmail.com>
2026-04-30 19:54:28 +0300

    update file with commented code on how one can structure query to include in fetch

--- forms/index.js
+++ forms/index.js
@@ -51,3 +51,33 @@ formGet.addEventListener("submit",(event)=>{
     })
     .catch(error=>console.log(error.message))
  })
+
+ /*
+ formGet.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const owner = document.querySelector("#owner-get").value;
+    const address = document.querySelector("#address-get").value;
+    const dogName = document.querySelector("#dogName-get").value;
+    const dogAge = document.querySelector("#dogAge-get").value;
+
+    let query = [];
+
+    if (owner) query.push(`owner=${owner}`);
+    if (address) query.push(`address=${address}`);
+    if (dogName) query.push(`dogName=${dogName}`);
+    if (dogAge) query.push(`dogAge=${dogAge}`);
+
+    const url = `http://localhost:3000/dogs?${query.join("&")}`;
+
+    console.log("Fetching:", url);
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        formGet.reset();
+    })
+    .catch(error => console.log(error.message));
+});
+*/
